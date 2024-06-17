79f07f58c5b15b2f5e87ee6c3bb180850f425ee9
admin <admin@DESKTOP-GR25COJ>
2024-06-17 13:48:05 +0530

    second commit

--- Test.java
+++ Test.java
@@ -1,12 +1,22 @@
 public class Test {
-  private int id;
+	private int id;
 
-public Test(){
+	public Test() {
+	}
+
+	public int getId() {
+	  return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.setId(10);
+		System.out.println(test.getId());
+	}
 }
-public int getId() {
-  this.id;
-}
-  public void setId(int id){
-    this.id=id;
-  }
-}
+
+
